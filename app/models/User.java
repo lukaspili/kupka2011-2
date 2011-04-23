5fb6653e465056083ace67aa716ec6928798ccdc
bargenson <derf4002@gmail.com>
2011-04-23 14:29:35 +0200

    Begin register

--- app/models/User.java
+++ app/models/User.java
@@ -1,16 +1,15 @@
 package models;
 
-<<<<<<< .merge_file_A16Kr4
 import com.google.gson.JsonObject;
 import play.db.jpa.Model;
 import play.mvc.Scope;
 
-=======
->>>>>>> .merge_file_q7Jha4
 import play.db.jpa.Model;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -18,6 +17,8 @@ import java.util.Date;
  */
 @Entity
 public class User extends Model {
+
+    private static DateFormat dateFormat = new SimpleDateFormat();
 
     public String email;
     public String firstName;
@@ -35,14 +36,15 @@ public class User extends Model {
     public static void register(String firstName, String lastName, String gender, String birthDate) {
 
     }
-<<<<<<< .merge_file_A16Kr4
 
     public static void facebookOAuthCallback(JsonObject data) {
+        String firstName = data.get("first_name").getAsString();
+        String lastName = data.get("last_name").getAsString();
+        String gender = data.get("gender").getAsString();
+        String birthDate = data.get("birthDate").getAsString();
+        String interestedIn = data.get("interested_in").getAsString();
 
+        register()
     }
     
 }
-
-=======
-}
->>>>>>> .merge_file_q7Jha4
