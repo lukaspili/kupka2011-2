ae8472255ff57658e3cf8e08d6a32c35cc23ef09
bargenson <derf4002@gmail.com>
2011-04-23 14:16:10 +0200

    Facebook Connect Integration

---
+++ app/models/User.java
@@ -0,0 +1,14 @@
+package models;
+
+import com.google.gson.JsonObject;
+import play.db.jpa.Model;
+import play.mvc.Scope;
+
+
+public class User extends Model {
+
+    public static void facebookOAuthCallback(JsonObject data) {
+
+    }
+
+}
